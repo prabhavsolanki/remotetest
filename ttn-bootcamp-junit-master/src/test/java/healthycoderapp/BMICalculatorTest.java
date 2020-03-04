454ac7498fbafca785255195f6092ca1f67cd402
prabhavsolanki <prabhavsolanki7@gmail.com>
2020-03-04 17:18:37 +0530

    Junit Testing Commit

---
+++ ttn-bootcamp-junit-master/src/test/java/healthycoderapp/BMICalculatorTest.java
@@ -0,0 +1,14 @@
+package healthycoderapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMICalculatorTest {
+
+    @Test
+    void fixMe(){
+        assertTrue(true);
+    }
+
+}
